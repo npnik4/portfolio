a9747c038bf057e0d3737bbf49f0af9aef82eccf
npnik4 <47047284+npnik4@users.noreply.github.com>
2021-03-10 01:29:40 -0500

    adding work info

--- v1/src/components/Grid/Card.js
+++ v1/src/components/Grid/Card.js
@@ -1,75 +1,57 @@
-import React, { Component } from 'react';
-import { Slug, Fade } from './Primitives';
-import {AnimateSharedLayout, motion} from 'framer-motion';
-import './Card.scss';
+/* eslint-disable react/jsx-no-target-blank */
+import React from "react";
+import { Slug, Fade } from "./Primitives";
+import { AnimateSharedLayout, motion } from "framer-motion";
+import "./Card.scss";
 
-export default class Card extends Component {
-  render() {
-    const { toggle, name, description, css, active } = this.props
-    return (
-      <AnimateSharedLayout>
+function Card(props) {
+  const { toggle, name, description, css, active, link, children } = props;
+
+  return (
+    <AnimateSharedLayout>
       <motion.div
         className={active ? "expandedCell" : "cell"}
-        style={{ backgroundImage: css, cursor: !active ? 'pointer' : 'auto' }}
-        onClick={!active ? toggle : undefined}>
+        style={{ backgroundImage: css, cursor: !active ? "pointer" : "auto" }}
+        onClick={!active ? toggle : undefined}
+      >
         <Fade show={active} delay={active ? 500 : 0}>
           <motion.div className="details">
             <Slug delay={600}>
               <div className="close">
                 <i
                   className="fas fa-times"
-                  style={{ cursor: 'pointer', fontSize: '32px', color: '#777777' }}
+                  style={{
+                    cursor: "pointer",
+                    fontSize: "32px",
+                    color: "#777777",
+                  }}
                   onClick={toggle}
                 />
               </div>
+              <div className="info-container">{children}</div>
               <h1>{name}</h1>
               <p>{description}</p>
+              <a href={link} target="_blank" className="button">
+                More information
+              </a>
             </Slug>
           </motion.div>
         </Fade>
-        
+
         <Fade
           show={!active}
-          from={{ opacity: 0, transform: 'translate3d(0,140px,0)' }}
-          enter={{ opacity: 1, transform: 'translate3d(0,0px,0)' }}
-          leave={{ opacity: 0, transform: 'translate3d(0,-50px,0)' }}
-          delay={active ? 0 : 400}>
+          from={{ opacity: 0, transform: "translate3d(0,140px,0)" }}
+          enter={{ opacity: 1, transform: "translate3d(0,0px,0)" }}
+          leave={{ opacity: 0, transform: "translate3d(0,-50px,0)" }}
+          delay={active ? 0 : 400}
+        >
           <motion.div className="default">
             <div style={{ zIndex: 1 }}>{name}</div>
           </motion.div>
         </Fade>
       </motion.div>
-      </AnimateSharedLayout>
-    )
-  }
+    </AnimateSharedLayout>
+  );
 }
 
-// export default class Card extends Component {
-//   state = { data }
-//   render() {
-//     return (
-//       <Grid
-//         className="grid"
-//         // Arbitrary data, should contain keys, possibly heights, etc.
-//         data={this.state.data}
-//         // Key accessor, instructs grid on how to fet individual keys from the data set
-//         keys={d => d.name}
-//         // Can be a fixed value or an individual data accessor
-//         heights={d => d.height}
-//         // Number of columns
-//         columns={2}
-//         // Space between elements
-//         margin={30}
-//         // Removes the possibility to scroll away from a maximized element
-//         lockScroll={false}
-//         // Delay when active elements (blown up) are minimized again
-//         closeDelay={500}
-//         // Regular react-spring configs
-//         config={config.slow}>
-//         {(data, active, toggle) => (
-//           <Cell {...data} active={active} toggle={toggle} />
-//         )}
-//       </Grid>
-//     )
-//   }
-// }
+export default Card;
--- v1/src/views/Feature/Feature.js
+++ v1/src/views/Feature/Feature.js
@@ -1,46 +1,65 @@
-import React, { useState } from 'react';
-import { Container } from '@material-ui/core';
-// import homeScreen from '../../assets/urRecalls/home_screen.png';
-// import ScanAvertGif from '../../assets/scanavert/X_frame.png';
-import Card from '../../components/Grid/Card';
-import './Feature.scss';
+import React, { useState } from "react";
+import { Container } from "@material-ui/core";
+import homeScreen from "../../assets/urRecalls/home_screen.png";
+import recall from "../../assets/urRecalls/recall.jpg";
+import logo from "../../assets/urRecalls/logo.png";
+import ScanAvert from "../../assets/scanavert/X_frame.png";
+import gif from "../../assets/scanavert/bCode_Fav.gif";
+import Card from "../../components/Grid/Card";
+import "./Feature.scss";
 
 function Feature() {
-    const [active, setActive] = useState([false, false]);
+  const [active, setActive] = useState([false, false]);
 
-    const toggle = (index) => {
-        setActive(Object.assign([], [false, false]));
-        const newActive = Object.assign([], active);
-        newActive[index] = !newActive[index];
-        setActive(newActive);
-    };
-    return (
-        <Container maxWidth="lg" className="feature" id="feature">
-            <h1 className="title">Featured <span>Projects</span>.</h1>
-            <div className="item-container">
-                <div className="item">
-                <Card
-                    toggle={() => toggle(0)}
-                    name="UrRecalls"
-                    description="In the UrRecalls app, you can scan the barcode of your product, or search by product name to get recall information."
-                    css="linear-gradient(-225deg, #E3FDF5 0%, #FFE6FA 100%)"
-                    active={active[0]}
-                />
-                </div>
-                <div className="item">
-                <Card
-                    toggle={() => toggle(1)}
-                    name="SCANAVERT"
-                    description="ScanAvert simplifies the process of making safer, compatible food choices."
-                    css="linear-gradient(120deg, #f6d365 0%, #fda085 100%)"
-                    active={active[1]}
-                />
-                </div>
-            </div>
+  const toggle = (index) => {
+    setActive(Object.assign([], [false, false]));
+    const newActive = Object.assign([], active);
+    newActive[index] = !newActive[index];
+    setActive(newActive);
+  };
 
-        </Container>
+  const imageStyles = {
+    objectFit: "contain",
+    maxWidth: "30%",
+    maxHeight: "100%",
+  };
 
-    )
+  return (
+    <Container maxWidth="lg" className="feature" id="feature">
+      <h1 className="title">
+        Featured <span>Projects</span>.
+      </h1>
+      <div className="item-container">
+        <div className="item">
+          <Card
+            toggle={() => toggle(0)}
+            name="UrRecalls"
+            description="In the UrRecalls app, you can scan the barcode of your product, or search by product name to get recall information."
+            css="linear-gradient(-225deg, #E3FDF5 0%, #FFE6FA 100%)"
+            active={active[0]}
+            link="https://npnik4.github.io/assets/docs/UrRecalls.pdf"
+          >
+            <img src={homeScreen} alt="urrecalls" style={imageStyles} />
+            <img src={logo} alt="urrecalls" style={imageStyles} className="extra-images"/>
+            <img src={recall} alt="urrecalls" style={imageStyles} className="extra-images"/>
+          </Card>
+        </div>
+        <div className="item">
+          <Card
+            toggle={() => toggle(1)}
+            name="SCANAVERT"
+            description="ScanAvert simplifies the process of making safer, compatible food choices."
+            css="linear-gradient(120deg, #f6d365 0%, #fda085 100%)"
+            active={active[1]}
+            link="https://npnik4.github.io/assets/docs/Scan%20Avert.pdf"
+          >
+            <img src={ScanAvert} alt="ScanAvert" style={imageStyles} />
+            <img src={gif} alt="ScanAvert" style={imageStyles} className="extra-images"/>
+          </Card>
+        </div>
+      </div>
+    </Container>
+  );
 }
 
 export default Feature;
